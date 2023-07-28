2914c1ef6b5c962d0a846dcf36d02d39c88014b8
Aslan0N <thenzrv@gmail.com>
2023-07-28 21:35:26 +0400

    Class, style

---
+++ vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1 @click="currnetColor = 'green'" :class="{ active : isActive}" :style="{ 'background-color': currnetColor}">{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      count:0,
+      name:"Aslan",
+      currnetColor:"gold",
+      isActive:true,
+    }
+  }
+}
+</script>
+
+<style>
+  h1{
+    text-align: center;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  }
+  .active{
+    border: 2px solid green;
+    border-radius: 4px;
+    /* background-color: #000; */
+    color: red;
+    font-size: 40px;
+  }
+</style>
